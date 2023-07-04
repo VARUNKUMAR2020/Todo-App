1bbe072eba7fef63ea1d9417aadad82e03a43538
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2023-07-05 01:35:22 +0530

    Todo Web-Application

---
+++ src/Components/Foot.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Foot = () => {
+  return (
+    <footer className="text-center mt-2 pt-2">
+      <div className=" fixed-bottom fw-bold">
+        CopyRights &copy; Desingned by{" "}
+        <span className="text-danger fs-4">VARUN</span>{" "}
+      </div>
+    </footer>
+  );
+};
+
+export default Foot;
